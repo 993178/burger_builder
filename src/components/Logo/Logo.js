b7f0cfbb176546bfd76c656f3ba92c8ea1647ed0
993178 <papilio29@hotmail.com>
2019-08-12 03:41:54 +0200

    logo erbij

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import burgerLogo from '../../assets/images/burger-logo.png';   // webpack gaat dit pad netjes inpakken en verschepen, maar zou dat niet doen als je dit pad direct in de <img src> zet
+import classes from './Logo.module.css';
 
 const logo = (props) => (
-    <div>
-        <img src={} />
+    <div className={classes.Logo} >
+        <img src={burgerLogo} alt="burger logo. Kinda weird to have an alt on such a visual website, does Discount Jonas plan to put such alts on every part of the burger too? I kinda doubt it tbh"/>
     </div>
 );
 
